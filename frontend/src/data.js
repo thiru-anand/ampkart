a3dd883797036bb7b268c70c9353743dd8bb075b
thiru-anand <78516825+thiru-anand@users.noreply.github.com>
2021-02-05 13:48:59 -0500

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    products:[
+        {
+          _id:'1',
+          name:'Reebokbag',   
+          category:'bags',
+          image:'/images/p1.jfif',
+          price:120,
+          brand:'reebok',
+          rating:4.5,
+          numReviews:10,
+          description:'high quality product',
+        },
+    ],
+};
+export default data;
